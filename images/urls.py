c872e76436210169db7c90cbfbecece087d19542
Naffik <fakenaffi@gmail.com>
2024-06-18 19:51:27 +0200

    Added image detail and image listing.

--- images/urls.py
+++ images/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import dashboard_view, ImageCreateView
+from .views import dashboard_view, ImageCreateView, ImageDetailView, ImageListView
 
 urlpatterns = [
     path('dashboard/', dashboard_view, name='dashboard'),
     path('image_upload/', ImageCreateView.as_view(), name='image_upload'),
+    path('image_detail/<int:pk>/', ImageDetailView.as_view(), name='image_detail'),
+    path('image_list/', ImageListView.as_view(), name='image_list'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
--- images/views.py
+++ images/views.py
@@ -3,8 +3,10 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views import View
+from django.views.generic import DetailView, ListView
 
 from images.forms import ImageForm
+from images.models import Image
 
 
 @login_required
@@ -29,9 +31,31 @@ class ImageCreateView(LoginRequiredMixin, View):
             new_image = image_form.save(commit=False)
             new_image.user = request.user
             new_image.save()
+            print(new_image.pk)
             messages.success(request, 'Dane zostały zaktualizowane')
         else:
             messages.error(request, 'Wystąpił błąd')
         return render(request,
                       self.template_name,
                       {'image_form': image_form})
+
+
+class ImageDetailView(LoginRequiredMixin, DetailView):
+    model = Image
+    template_name = 'images/image_detail.html'
+    context_object_name = 'image'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['previous_url'] = self.request.META.get('HTTP_REFERER', '')
+        return context
+
+
+class ImageListView(LoginRequiredMixin, ListView):
+    model = Image
+    template_name = 'images/image_list.html'
+    context_object_name = 'images'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Image.objects.filter(user=self.request.user).order_by('-created_at')
