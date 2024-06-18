0f7525bbff30079ab29e2b6e2906a1879ba17935
Naffik <fakenaffi@gmail.com>
2024-06-18 20:17:23 +0200

    Added account_tier field to profile model.

--- account/models.py
+++ account/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.conf import settings
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True,
-                                     null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d',
-                              blank=True)
-
-    def __str__(self):
-        return 'Profil użytkownika {}'.format(self.user.username)
-
-
 class AccountTier(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     thumbnail_size = models.CharField(max_length=255, null=False, blank=False)
@@ -24,3 +12,17 @@ class AccountTier(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True,
+                                     null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d',
+                              blank=True)
+    account_tier = models.ForeignKey(AccountTier, on_delete=models.CASCADE, null=True, blank=False)
+
+    def __str__(self):
+        return 'Profil użytkownika {}'.format(self.user.username)
+
